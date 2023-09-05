7efa45f84591cd3f4f78c3d157b6ec1c688ee4fc
Deep838 <deepmondal838@gmail.com>
2023-09-05 08:56:11 +0530

    day 5

---
+++ python day_5.py
@@ -0,0 +1,15 @@
+import time
+
+count= 1
+
+while count<11:
+    print(count)
+    count += 1
+    time.sleep(0.3)
+
+
+list = ["Raman","Kalam","S.N Bose","Homi Bhabha","Sarabhai","Ramakrishnan","C.N Rao","Ardaseer Cursetjee"]
+
+for names in list:
+    print(names," was a great scientist.")
+    time.sleep(0.3)

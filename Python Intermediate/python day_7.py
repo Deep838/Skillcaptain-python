c6a6704d92ab0e18218639ac9cdbeb62dd796fd3
Deep838 <deepmondal838@gmail.com>
2023-09-19 16:04:33 +0530

    Python II day 7

--- Python Intermediate/python day_7.py
+++ Python Intermediate/python day_7.py
@@ -1,18 +1,24 @@
+#1. Create a user class
+
 class User:
+
+    #2 & 3 class attributes and parameteres
     def __init__(self, username, email, password):
         self.username = username
         self.email = email
         self.password = password
 
+#4. users info
     def user_info(self):
         print(f"Your username is {self.username} and email is {self.email}")
-
+#6. validating the email adress to be unique
     def valid_email(email, users):
         for user in users:
             if user.email == email:
                 return False
         return True
 
+#5. collect user info and create a new user object
     def register_user(users):
         print("User Registration")
         while True:
@@ -28,6 +34,7 @@ class User:
             print("User registered successfully!")
             break
 
+#7. storing the user info in a list
 users = []
 while True:
     User.register_user(users)

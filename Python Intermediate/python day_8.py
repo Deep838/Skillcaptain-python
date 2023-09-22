2619fedd380c2f8c5a4793c5525b8f5bd9bbb569
Deep838 <deepmondal838@gmail.com>
2023-09-22 23:53:55 +0530

    Python II day 8

---
+++ Python Intermediate/python day_8.py
@@ -0,0 +1,41 @@
+#1. Create a Product class
+
+class Product:
+
+    #2 & 3 class attributes and parameteres
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = int(price)
+        self.quantity = iquantity
+
+#4. product's info
+    def product_info(self):
+        print(f"product {self.name}'s price is {self.price} and quantity {self.quantity}")
+
+#5. collect Product info and create a new product object
+    def register_product(products):
+        print("Product Registration")
+        while True:
+            name = input("Enter product name: ")
+            price = input("Enter product price: ")
+            quantity = input("Enter quantity: ")
+
+            new_product = Product(name, price, quantity)
+            products.append(new_product)
+            print("Product registered successfully!")
+            break
+
+#7. storing the products info in a list
+products = []
+while True:
+    Product.register_product(products)
+    another = input("Do you want to register another Product? (yes/no): ")
+    if another.lower() != "yes":
+        break
+
+print("\nRegistered Products:")
+for idx, product in enumerate(products, start=1):
+    print(f"Product: {idx}")
+    print(f"Name: {product.name}")
+    print(f"Price: {product.price}")
+    print(f"quantity: {product.quantity}")

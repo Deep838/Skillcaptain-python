5b0398a322a780c83cebed161e52020c5a0074aa
Deep838 <deepmondal838@gmail.com>
2023-10-05 02:13:53 +0530

    python_day3

--- Python Advanced/python day_3/python day_3.py
+++ Python Advanced/python day_3/python day_3.py
@@ -1,13 +1,10 @@
-with open("cars.txt", "r") as file:
-    car_types = iter(file)
+file = open("cars.txt","r")
+cars = iter(file)
 
-
-    try:
-        # Use next() to iterate through each line
-        while True:
-            line = next(car_types)
-            # Process the line
-            print(line)
-
-    except StopIteration:
-        pass
+print(next(cars))
+print(next(cars))
+print(next(cars))
+print(next(cars))
+print(next(cars))
+print(next(cars))
+print(next(cars))

965507fd6db9ad4f2ef1f5b3220b62a78914b47f
Deep838 <deepmondal838@gmail.com>
2023-10-05 00:04:35 +0530

    python_day2

---
+++ Python Advanced/python day_2/main.py
@@ -0,0 +1,9 @@
+#importing the math_operations
+import math_operations
+
+#using the functions in the module
+print(math_operations.add(34,45))
+print(math_operations.subtract(45,34))
+print(math_operations.multiply(45,34))
+print(math_operations.divide(45,5))
+print(math_operations.power(34,5))
